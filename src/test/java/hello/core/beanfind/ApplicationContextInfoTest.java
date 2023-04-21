2b8cbb513f9503e9c8ffe5c5a670b1f6b7a234e5
이종민 <102534186+lamodadite@users.noreply.github.com>
2023-04-22 04:31:34 +0900

    initial commit

---
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,44 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findAllBean() {
+        //given
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        //when
+        //then
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("name = " + beanDefinitionName + ", object = " + bean);
+        }
+    }
+
+    @Test
+    @DisplayName("모든 빈 출력하기")
+    void findApplicationBean() {
+        //given
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        //when
+        //then
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + ", object = " + bean);
+            }
+        }
+    }
+
+}
